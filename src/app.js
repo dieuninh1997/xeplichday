125019165cdfa10503b7425665ff59584b9ad355
HuyManh <huymanhtmhp@gmail.com>
2019-03-16 11:23:01 +0700

    Add views

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
+const path = require('path')
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const app = express()
 
+app.use(express.static(path.resolve(__dirname, './public')))
+
+app.set('views', path.resolve(__dirname, './views'))
+nunjucks.configure(path.resolve(__dirname, './views'), {
+  autoescape: true,
+  express: app
+})
+app.set('view engine', 'html')
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.render('home/home.html',
+    { a: 'ss' }
+  )
 })
 
 module.exports = app
